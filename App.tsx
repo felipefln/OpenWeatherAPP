c843982bf42f50f898729a39bc74399e7e54ab7c
Felipe Weiduschadt de Carvalho <felipe.carvalhofln@gmail.com>
2023-01-09 23:43:28 -0300

    chore: add core app render

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+import { Provider } from "react-redux";
+
+import { store } from "./src/store";
+// import { Button } from "./src/components/Button";
+// import { Spinner } from "./src/components/Spinner";
+// import { ReloadIcon } from "./src/components/ReloadIcon";
+// import { PreviousSearch } from "./src/components/PreviousSearch";
+import { Routes } from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Routes />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });
